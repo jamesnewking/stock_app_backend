672486b393fb8d59ef1bcb048255b03d7039c7e4
James <jamesnewking@gmail.com>
2021-03-17 20:33:07 -0700

    gets company overview/details

--- routes/stocks.js
+++ routes/stocks.js
@@ -3,5 +3,6 @@ const express = require('express');
 const StocksController = express.Router();
 
 StocksController.get('/getstockticker/:ticker', require('../controllers/stocks/getStockTicker'));
+StocksController.get('/getcompanyoverview/:ticker', require('../controllers/stocks/getCompanyOverview'));
 
 module.exports = StocksController;
